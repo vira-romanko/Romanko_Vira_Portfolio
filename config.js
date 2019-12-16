32bc9de571648df03012995d35e1a9604cf21b36
Vira <vira.romanko99@gmail.com>
2019-12-16 07:19:58 -0500

    data

--- config.js
+++ config.js
@@ -1,9 +1,9 @@
 const config = {
-    host: "localhost",
-    port: 8889, // 8889 for mac
-    uname: "root",
-    pword: "root", // leave blank for windows
-    database: "db_portfolio"
+    host: "us-cdbr-iron-east-05.cleardb.net",
+    //port: 8889, // 8889 for mac
+    uname: "ba457f6494c2af",
+    pname: "3359ff86", // leave blank for windows
+    database: "heroku_b54144f084d58f9"
 }
 
 module.exports = config;
--- routes/index.js
+++ routes/index.js
@@ -2,42 +2,55 @@ var express = require('express');
 var router = express.Router();
 
 
-const sql = require('../utils/sql');
+const connect = require('../utils/sql');
 
 
 
 router.get('/', (req,res)=>{
   console.log("at the main route");
+  connect.getConnection((err, connection) => {
+    if (err){
+      return 
+      console.log(error.message);
+  }
  let query = "Select ID, image, name FROM tbl_projects";
- sql.query(query, (err,result)=>{
+ connect.query(query, (err,result)=>{
      console.log("getting data")
      if (err){
          throw err;
          console.log(err);
      }
-     console.log(result);
+     //console.log(result);
     
     res.render('home' , {portfolio:result});
     
-    
- })
+    }); 
+ });
 })
 
 
 
-router.get('/portfolio/:id', (req, res)=> {
+router.get('/users/:id', (req, res)=> {
   console.log("hit a dynamic route");
   console.log(req.params.id);
 
   //here is where we set up query
   let query =`SELECT * FROM tbl_projects WHERE ID="${req.params.id}"`;
-  sql.query(query, (err, result)=>{
+  connect.query(query, (err, result)=>{
+    connection.release();
     console.log('getting data');
     if (err) console.log(err);//somthing done broke!
 
+    result[0].tags = result[0].tags.split(',').map(function(item) {
+      item = item.trim(); //remove the extra spaces from each word
+
+      return item;
+   });
+ console.log('after split: ', result[0]);
      
-    console.log(result);//send that row back to the calling function
-    res.json(result[0]);
+    //console.log(result);//send that row back to the calling function
+    //return(result);
+  res.json(result[0]);
     
  })
  
